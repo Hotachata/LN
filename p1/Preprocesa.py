bc4af0a4a95afe57b461cb21bbe14583e438aa59
Ariadna <achicol1900@alumno.ipn.mx>
2025-03-02 12:41:23 -0600

    cambia ñ por n y añade espacios después del punto

--- p1/Preprocesa.py
+++ p1/Preprocesa.py
@@ -10,7 +10,7 @@ class Preprocesa:
         for i in especiales:
             text.replace(i," ")
         # (le quité el espacio a forbbiden)
-        forbidden = {"?" , "¿" , "¡" , "!" , "<" , ">" , "(" , ")" , "\"" , "." , "," , ":" , ";" , "-" , "&" , "@" , "/" , "N/A" , "#" , "$"}
+        forbidden = {"?" , "¿" , "¡" , "!" , "<" , ">" , "(" , ")" , "\"" , "," , ":" , ";" , "-" , "&" , "@" , "/" , "N/A" , "#" , "$"}
         pf = set(string.punctuation).union(forbidden) # caracteres prohibidos (unión de punctuation y forbidden)
         punctuationfree="".join([i  for i in text if i not in pf]) # nueva cadena sin los caracteres prohibidos
         return punctuationfree.strip()
@@ -26,6 +26,8 @@ class Preprocesa:
             ("í", "i"),
             ("ó", "o"),
             ("ú", "u"),
+            ("ñ", "n"),
+            (".", " ")
             )
         for a, b in replacements:
             s = s.replace(a, b).replace(a.upper(), b.upper())
@@ -33,14 +35,24 @@ class Preprocesa:
     
     # método para preprocesar el texto
     def preprocesamiento(self, text):
-        text = self.quitarAcentos(text)
+        text = self.quitarAcentos(text) #quita acentos, ñ y añade espacios después del .
         text = self.lower_words(text)
         text = self.remove_punctuation(text)
         return text
 
-df = pd.read_csv("news.csv") # dataframe
-preprocesador = Preprocesa()
-# preprocesamiento para news (nueva columna)
-df["preprocesado"] = df["news"].apply(preprocesador.preprocesamiento)
-# nuevo archivo CSV
-df.to_csv("news1.csv", index=False)
+try:
+    df = pd.read_csv("news.csv") # dataframe, encoding='utf-8'
+    preprocesador = Preprocesa()
+    # preprocesamiento en la columna news (creamos nueva columna "preprocesado")
+    df["preprocesado"] = df["news"].apply(preprocesador.preprocesamiento)
+    # nuevo archivo CSV
+    df.to_csv("news1.csv", index=False)
+    print("\nArchivo preprocesado!\n")
+    
+    # leemos el nuevo archivo con el texto filtrado
+    df = pd.read_csv("news1.csv") # reescribimos dataframe
+    palabras = df["preprocesado"]
+    print(palabras[1])
+    
+except Exception as e:
+    print(f"\nHa ocurrido un error: {e}\n")
